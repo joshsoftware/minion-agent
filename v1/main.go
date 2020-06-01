f3dadc81b325a9c142e0cb8534ac5b333b4deac0
J. Austin Hughey <jah@jah.io>
2020-06-01 14:18:06 -0600

    move log tailing into its own package

--- v1/main.go
+++ v1/main.go
@@ -1,33 +1,14 @@
 package main
 
 import (
-	"io"
 	"joshsoftware/minion-agent/config"
-	"log"
+	"joshsoftware/minion-agent/logs"
 	"os"
-
-	"github.com/nxadm/tail"
 )
 
 func main() {
-	seekloc := tail.SeekInfo{Offset: 0, Whence: io.SeekEnd}
 	config := config.ReadConfig(os.Getenv("CONFIG"))
-
-	for _, l := range config.Logs {
-		t, err := tail.TailFile(l, tail.Config{Follow: true, Location: &seekloc})
-		if err != nil {
-			// If the log file doesn't exist, don't crash, just report it
-			// TODO: Put this in another package and call a more robust error handling system
-			log.Println("WARN: ", err)
-		}
-
-		go func() {
-			for line := range t.Lines {
-				log.Println(line.Text)
-			}
-		}()
-	}
-
+	logs.TailLogs(config)
 	for {
 	}
 }
